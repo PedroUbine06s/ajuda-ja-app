73863c0291e70b26f75a303130c8f53ea3230b0b
Pedro Ubine <pedroubine@hotmail.com>
2025-06-08 18:28:10 -0300

    format api

--- services/api.ts
+++ services/api.ts
@@ -1,24 +1,23 @@
-const BASE_URL = 'http://10.0.2.2:3000';
+const BASE_URL = "http://10.0.2.2:3000";
 
 export interface ApiService {
-    id: number;
-name: string;
+  id: number;
+  name: string;
 }
 
 export const getServices = async (): Promise<ApiService[]> => {
-try {
-const response = await fetch(`${BASE_URL}/services`);
-if (!response.ok) {
-      throw new Error('Failed to fetch services from the API.');
+  try {
+    const response = await fetch(`${BASE_URL}/services`);
+    if (!response.ok) {
+      throw new Error("Failed to fetch services from the API.");
     }
     const data: ApiService[] = await response.json();
     return data;
   } catch (error) {
-    console.error('API Error:', error);
+    console.error("API Error:", error);
     throw error;
   }
 };
-
 
 export interface CreateUserPayload {
   name: string;
@@ -27,48 +26,47 @@ export interface CreateUserPayload {
   phone: string;
   address: string;
   password: string;
-  userType: 'COMMON' | 'PROVIDER';
+  userType: "COMMON" | "PROVIDER";
   serviceIds?: number[];
   profilePictureUrl?: string;
 }
 
 export interface User {
-    user : {
-          id: number;
-          name: string;
-          email: string;
-          userType: 'COMMON' | 'PROVIDER';
-          dateOfBirth: string;
-          phone: string;
-          address: string;
-          location?: {
-              type: string,
-              coordinates: number[]
-          }
-        },
-    toke: string;
+  user: {
+    id: number;
+    name: string;
+    email: string;
+    userType: "COMMON" | "PROVIDER";
+    dateOfBirth: string;
+    phone: string;
+    address: string;
+    location?: {
+      type: string;
+      coordinates: number[];
+    };
+  };
+  toke: string;
 }
 
-export const createAccount = async (payload: CreateUserPayload): Promise<User> => {
+export const createAccount = async (
+  payload: CreateUserPayload
+): Promise<User> => {
   try {
     const response = await fetch(`${BASE_URL}/auth/register`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(payload),
     });
 
-
     if (!response.ok) {
       const errorData = await response.json();
 
-      throw new Error(errorData.message || 'Houve um erro ao criar o usuário.');
+      throw new Error(errorData.message || "Houve um erro ao criar o usuário.");
     }
 
-
     return await response.json();
-
   } catch (error) {
     console.error("Error in createAccount API call:", error);
 
@@ -76,26 +74,29 @@ export const createAccount = async (payload: CreateUserPayload): Promise<User> =
   }
 };
 
-export const loginIntoAccount = async (payload: {email: string, password: string}): Promise<User> => {
-    try {
-        const response = await fetch(`${BASE_URL}/auth/login`, {
-            method: 'POST',
-            headers: {
-                'Content-Type' : 'application/json',
-            },
-            body: JSON.stringify(payload)
-         })
+export const loginIntoAccount = async (payload: {
+  email: string;
+  password: string;
+}): Promise<User> => {
+  try {
+    const response = await fetch(`${BASE_URL}/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
 
-        if(!response.ok) {
-            const errorData = await response.json()
+    if (!response.ok) {
+      const errorData = await response.json();
 
-            throw new Error(errorData.message || 'Houve um erro ao fazer o login')
-        }
-
-        return await response.json();
-    } catch (error) {
-        console.error("Error in login API call:", error);
-
-        throw error
+      throw new Error(errorData.message || "Houve um erro ao fazer o login");
     }
-}
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error in login API call:", error);
+
+    throw error;
+  }
+};
